c3cbf82a43db2366eeac1d033975e76ca46d1995
Luca Rosellini <luca@fintonic.com>
2014-12-11 09:37:28 +0100

    build scripts refactor

---
+++ kina-scripts/build-kina-release.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+# Kina Deployment script
+
+KINA_REPO="git@github.com:lucarosellini/kina.git"
+
+echo " >>> Kina DEPLOYMENT <<< "
+
+TAGVERSION="false"
+
+LOCAL_EDITOR=$(which vim)
+
+if [ -z "$LOCAL_EDITOR" ]; then
+    $LOCAL_EDITOR=$(which vi)
+fi
+
+if [ -z "$LOCAL_EDITOR" ]; then
+    echo "Cannot find any command line editor, ChangeLog.txt won't be edited interactively"
+fi
+
+LOCAL_DIR=`pwd`
+
+echo "LOCAL_DIR=$LOCAL_DIR"
+
+TMPDIR=/tmp/kina-clone
+
+rm -rf ${TMPDIR}
+mkdir -p ${TMPDIR}
+
+mvn -version >/dev/null || { echo "Cannot find Maven in path, aborting"; exit 1; }
+
+#### Create Kina jars from github (master tag) through maven release plugin
+
+# Clone Kina (master tag) from github
+git clone ${KINA_REPO} ${TMPDIR} || { echo "Cannot clone kina project"; exit 1; }
+cd ${TMPDIR}
+
+git checkout develop
+
+echo "Generating version number"
+RELEASE_VER=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version 2>/dev/null | grep -v '\[' | sed s/\-SNAPSHOT//) || { echo "Cannot generate next version number"; exit 1; }
+
+cd ${TMPDIR}/
+
+echo "Updating pom version numbers"
+cd ${TMPDIR}/
+mvn -q versions:set -DnewVersion=${RELEASE_VER} || { echo "Cannot modify pom file with next version number"; exit 1; }
+
+echo "Building Kina ${RELEASE_VER} ..."
+mvn -q clean package -DskipTests || { echo "Cannot build Kina $RELEASE_VER "; exit 1; }
+#mvn -q test || { echo "Tests failed for Kina $RELEASE_VER "; exit 1; }
+
+#fix version number for travis CI
+sed -i -e s/\?branch=develop\)/\?branch=version-${RELEASE_VER}/ README.md
+
+find . -name 'pom.xml.versionsBackup' | xargs rm
+
+# copy libs
+mkdir -p ${TMPDIR}/lib || { echo "Cannot create output lib directory"; exit 1; }
+
+cp  -b ./kina-agent/target/*.jar ${TMPDIR}/lib || { echo "Cannot copy kina-agent jars to output lib directory, aborting"; exit 1; }
+cp  -b ./kina-commons/target/*.jar ${TMPDIR}/lib || { echo "Cannot copy kina-commons jars to output lib directory, aborting"; exit 1; }
+cp  -b ./kina-cassandra/target/*.jar ${TMPDIR}/lib || { echo "Cannot copy kina-cassandra jars to output lib directory, aborting"; exit 1; }
+cp  -b ./kina-cassandra/target/alternateLocation/*.jar ${TMPDIR}/lib || { echo "Cannot copy kina-cassandra alternate jars to output lib directory, aborting"; exit 1; }
+cp  -b ./kina-mongodb/target/*.jar ${TMPDIR}/lib || { echo "Cannot copy kina-mongodb jars to output lib directory, aborting"; exit 1; }
+cp  -b ./kina-mongodb/target/alternateLocation/*.jar ${TMPDIR}/lib || { echo "Cannot copy kina-mongodb alternate jars to output lib directory, aborting"; exit 1; }
+
+rm -f ${TMPDIR}/lib/*.jar~
+
+if [ "false" == $TAGVERSION ]; then
+    echo "Kina ${RELEASE_VER} built successfully"
+    exit 0
+fi
+
+git flow init -d || { echo "Cannot initialize git flow in kina-clone project"; exit 1; }
+git flow release start version-$RELEASE_VER || { echo "Cannot create $RELEASE_VER branch"; exit 1; }
+
+git commit -a -m "[kina release prepare] preparing for version ${RELEASE_VER}"  || { echo "Cannot commit changes in kina-clone project"; exit 1; }
+
+echo " >>> Uploading new release branch to remote repository"
+git flow release publish version-$RELEASE_VER || { echo "Cannot publish $RELEASE_VER branch"; exit 1; }
+
+
+# Generating ChangeLog
+git fetch --tags
+latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
+
+touch ChangeLog.txt
+git add ChangeLog.txt
+
+echo -e "[${RELEASE_VER}]\n\n$(git log ${latest_tag}..HEAD)\n\n$(cat ChangeLog.txt)" > ChangeLog.txt
+
+if [ -n "$LOCAL_EDITOR" ]; then
+    $LOCAL_EDITOR ChangeLog.txt
+fi
+
+echo "Finishing release ${RELEASE_VER}"
+mvn -q clean
+
+git commit -a -m "[Updated ChangeLog.txt for release ${RELEASE_VER}]"
+
+echo "Generating next SNAPSHOT version"
+curr_version=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version 2>/dev/null | grep -v '\[')
+major=$(echo $curr_version | cut -d "." -f 1)
+minor=$(echo $curr_version | cut -d "." -f 2)
+bugfix=$(echo $curr_version | cut -d "." -f 3)
+
+next_bugfix=$(expr $bugfix + 1)
+
+next_version="${major}.${minor}.${next_bugfix}-SNAPSHOT"
+
+echo "Next SNAPSHOT version: ${next_version}"
+
+echo "Finishing release"
+git flow release finish -k -mFinishing_Release_$RELEASE_VER version-$RELEASE_VER || { echo "Cannot finish Kina ${next_version}"; exit 1; }
+git push --tags
+
+git checkout master
+git push origin || { echo "Cannot push to master"; exit 1; }
+
+git checkout develop
+
+echo "Setting new snapshot version"
+mvn versions:set -DnewVersion=${next_version} || { echo "Cannot set new version: ${next_version}"; exit 1; }
+
+#fix version number for travis CI
+sed -i -e s/\?branch=version-${RELEASE_VER}/\?branch=develop\)/ README.md
+
+find . -name 'pom.xml.versionsBackup' | xargs rm
+
+echo "Commiting version ${next_version}"
+git commit -a -m "[kina release finish] next snapshot version ${next_version}" || { echo "Cannot commit new changes for ${next_version}"; exit 1; }
+
+git push origin || { echo "Cannot push new version: ${next_version}"; exit 1; }
+
+echo "Success: new version tagged in github as 'version-${RELEASE_VER}'"
--- kina-scripts/package-spark-kina-distribution.sh
+++ kina-scripts/package-spark-kina-distribution.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # Kina Deployment script
 
-KINA_REPO="git@github.com:lucarosellini/kina.git"
-
-echo " >>> Kina DEPLOYMENT <<< "
-
 SPARK_REPO="$1"
 
 if [ -z "$1" ]; then
@@ -14,10 +10,10 @@ fi
 SPARK_BRANCH="$2"
 
 if [ -z "$2" ]; then
-    SPARK_BRANCH="v1.1.0"
+    SPARK_BRANCH="v1.1.1"
 fi
 
-LOCAL_EDITOR=$(which vim)
+LOCAL_EDITOR=$(which vi)
 
 if [ -z "$LOCAL_EDITOR" ]; then
     $LOCAL_EDITOR=$(which vi)
@@ -34,110 +30,13 @@ echo "LOCAL_DIR=$LOCAL_DIR"
 TMPDIR=/tmp/kina-clone
 TMPDIR_SPARK=/tmp/spark-clone
 
-rm -rf ${TMPDIR}
-mkdir -p ${TMPDIR}
+cd ${TMPDIR}
+RELEASE_VER=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version 2>/dev/null | grep -v '\[' | sed s/\-SNAPSHOT//) || { echo "Cannot generate next version number"; exit 1; }
 
 rm -rf ${TMPDIR_SPARK}
 mkdir -p ${TMPDIR_SPARK}
 
 mvn -version >/dev/null || { echo "Cannot find Maven in path, aborting"; exit 1; }
-
-#### Create Kina jars from github (master tag) through maven release plugin
-
-# Clone Kina (master tag) from github
-git clone ${KINA_REPO} ${TMPDIR} || { echo "Cannot clone kina project"; exit 1; }
-cd ${TMPDIR}
-
-git checkout develop
-
-echo "Generating version number"
-RELEASE_VER=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version 2>/dev/null | grep -v '\[' | sed s/\-SNAPSHOT//) || { echo "Cannot generate next version number"; exit 1; }
-
-cd ${TMPDIR}/
-
-git flow init -d || { echo "Cannot initialize git flow in kina-clone project"; exit 1; }
-git flow release start version-$RELEASE_VER || { echo "Cannot create $RELEASE_VER branch"; exit 1; }
-
-echo "Updating pom version numbers"
-cd ${TMPDIR}/
-mvn -q versions:set -DnewVersion=${RELEASE_VER} || { echo "Cannot modify pom file with next version number"; exit 1; }
-
-echo "Building Kina ${RELEASE_VER} ..."
-mvn -q clean package -DskipTests || { echo "Cannot build Kina $RELEASE_VER "; exit 1; }
-mvn -q test || { echo "Tests failed for Kina $RELEASE_VER "; exit 1; }
-
-#fix version number for travis CI
-sed -i -e s/\?branch=develop\)/\?branch=version-${RELEASE_VER}/ README.md
-
-find . -name 'pom.xml.versionsBackup' | xargs rm
-
-git commit -a -m "[kina release prepare] preparing for version ${RELEASE_VER}"  || { echo "Cannot commit changes in kina-clone project"; exit 1; }
-
-echo " >>> Uploading new release branch to remote repository"
-git flow release publish version-$RELEASE_VER || { echo "Cannot publish $RELEASE_VER branch"; exit 1; }
-
-mkdir -p ${TMPDIR}/lib || { echo "Cannot create output lib directory"; exit 1; }
-
-cp  -b ./kina-cassandra/target/*.jar ${TMPDIR}/lib || { echo "Cannot copy cassandra target jars to output lib directory, aborting"; exit 1; }
-cp  -b ./kina-cassandra/target/alternateLocation/*.jar ${TMPDIR}/lib || { echo "Cannot copy cassandra alternate target jars to output lib directory, aborting"; exit 1; }
-cp  -b ./kina-mongodb/target/*.jar ${TMPDIR}/lib || { echo "Cannot copy mongodb target jars to output lib directory, aborting"; exit 1; }
-cp  -b ./kina-mongodb/target/alternateLocation/*.jar ${TMPDIR}/lib || { echo "Cannot copy mongodb alternate jars to output lib directory, aborting"; exit 1; }
-
-rm -f ${TMPDIR}/lib/*.jar~
-
-# Generating ChangeLog
-git fetch --tags
-latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
-
-touch ChangeLog.txt
-git add ChangeLog.txt
-
-echo -e "[${RELEASE_VER}]\n\n$(git log ${latest_tag}..HEAD)\n\n$(cat ChangeLog.txt)" > ChangeLog.txt
-
-if [ -n "$LOCAL_EDITOR" ]; then
-    $LOCAL_EDITOR ChangeLog.txt
-fi
-
-echo "Finishing release ${RELEASE_VER}"
-mvn -q clean
-
-git commit -a -m "[Updated ChangeLog.txt for release ${RELEASE_VER}]"
-
-echo "Generating next SNAPSHOT version"
-curr_version=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version 2>/dev/null | grep -v '\[')
-major=$(echo $curr_version | cut -d "." -f 1)
-minor=$(echo $curr_version | cut -d "." -f 2)
-bugfix=$(echo $curr_version | cut -d "." -f 3)
-
-next_bugfix=$(expr $bugfix + 1)
-
-next_version="${major}.${minor}.${next_bugfix}-SNAPSHOT"
-
-echo "Next SNAPSHOT version: ${next_version}"
-
-echo "Finishing release"
-git flow release finish -k -mFinishing_Release_$RELEASE_VER version-$RELEASE_VER || { echo "Cannot finish Kina ${next_version}"; exit 1; }
-git push --tags
-
-git checkout master
-git push origin || { echo "Cannot push to master"; exit 1; }
-
-git checkout develop
-
-echo "Setting new snapshot version"
-mvn versions:set -DnewVersion=${next_version} || { echo "Cannot set new version: ${next_version}"; exit 1; }
-
-#fix version number for travis CI
-sed -i -e s/\?branch=version-${RELEASE_VER}/\?branch=develop\)/ README.md
-
-find . -name 'pom.xml.versionsBackup' | xargs rm
-
-echo "Commiting version ${next_version}"
-git commit -a -m "[kina release finish] next snapshot version ${next_version}" || { echo "Cannot commit new changes for ${next_version}"; exit 1; }
-
-git push origin || { echo "Cannot push new version: ${next_version}"; exit 1; }
-
-echo "RELEASE_VER=$RELEASE_VER"
 
 # Clone spark repo from github
 git clone ${SPARK_REPO} ${TMPDIR_SPARK} || { echo "Cannot clone spark repo from github"; exit 1; }
@@ -151,8 +50,8 @@ patch -p1 < ${TMPDIR}/kina-scripts/spark-patches/kina-spark-${SPARK_BRANCH}-patc
 chmod +x bin/kina-shell
 
 echo " >>> Executing make distribution script"
-##  --skip-java-test has been added to Spark 1.0.0, avoids prompting the user about not having JDK 6 installed
-./make-distribution.sh --name --skip-java-test -Phadoop-2.4 -Pyarn || { echo "Cannot make Spark distribution"; exit 1; }
+##  --skip-java-test has been added to Spark 1.1.1, avoids prompting the user about not having JDK 6 installed
+./make-distribution.sh --name --skip-java-test -DskipTests -Phadoop-2.4 -Pyarn || { echo "Cannot make Spark distribution"; exit 1; }
 
 DISTDIR=kina-distribution-${RELEASE_VER}
 DISTFILENAME=${DISTDIR}.tgz
@@ -179,12 +78,12 @@ echo " >>> Finishing"
 cd ..
 
 # Delete cloned spark project
-rm -rf ${TMPDIR}  || { echo "Cannot remove kina-clone project"; exit 1; }
+# rm -rf ${TMPDIR}  || { echo "Cannot remove kina-clone project"; exit 1; }
 
 # Delete cloned Kina project
-rm -rf ${TMPDIR_SPARK}  || { echo "Cannot remove spark-clone project"; exit 1; }
+# rm -rf ${TMPDIR_SPARK}  || { echo "Cannot remove spark-clone project"; exit 1; }
 
 cd "$LOCAL_DIR"/..
-git checkout develop
+# git checkout develop
 
 echo " >>> SCRIPT EXECUTION FINISHED <<< "
